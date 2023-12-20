cc3e0ef233d27ca28f9b83c7f1e97f8fc3b35430
alamgir-napollo <130369546+alamgir-napollo@users.noreply.github.com>
2023-12-20 12:19:15 +0500

    Radio buttons added to basic salary

--- Modules/Hrm/Resources/views/setsalary/basic_salary.blade.php
+++ Modules/Hrm/Resources/views/setsalary/basic_salary.blade.php
@@ -2,9 +2,26 @@
 <div class="modal-body">
     <div class="row">
         <div class="col-md-12">
-            <div class="form-group">
+            {{-- <div class="form-group">
                 {{ Form::label('salary_type', __('Payslip Type'), ['class' => 'form-label']) }}
                 {{ Form::select('salary_type', $payslip_type, null, ['class' => 'form-control ', 'required' => 'required', 'placeholder' => 'Select Payslip Type']) }}
+            </div> --}}
+            <div class="form-group">
+                {{ Form::label('salary_type', __('Payslip Type'), ['class' => 'form-label']) }}
+                <div class="form-check">
+                    {{ Form::radio('salary_type', 'per_month', true, ['class' => 'form-check-input', 'id' => 'per_month']) }}
+                    {{ Form::label('per_month', 'Per Month', ['class' => 'form-check-label']) }}
+                </div>
+                
+                <div class="form-check">
+                    {{ Form::radio('salary_type', 'per_week', false, ['class' => 'form-check-input', 'id' => 'per_week']) }}
+                    {{ Form::label('per_week', 'Per Week', ['class' => 'form-check-label']) }}
+                </div>
+                
+                <div class="form-check">
+                    {{ Form::radio('salary_type', 'per_hour', false, ['class' => 'form-check-input', 'id' => 'per_hour']) }}
+                    {{ Form::label('per_hour', 'Per Hour', ['class' => 'form-check-label']) }}
+                </div>
             </div>
         </div>
         <div class="col-md-12">
